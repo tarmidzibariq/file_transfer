045de68930249c0c615a62324148de2a7190a9bc
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2026-05-10 08:12:47 +0700

    first commit

---
+++ myapp/app/Models/File.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'original_name',
+        'file_name',
+        'file_size',
+        'mime_type',
+        'path',
+        'is_public',
+    ];
+
+    // Relationships user
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ myapp/database/migrations/2026_05_09_083942_create_files_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('original_name', 100);
+            $table->string('file_name', 65);
+            $table->bigInteger('file_size');
+            $table->string('mime_type', 10);
+            $table->string('path', 255);
+            $table->string('is_public')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('files');
+    }
+};
